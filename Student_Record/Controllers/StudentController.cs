96159280bc76b6df39a5b9ecf53bb0b855b20505
Akshit Dhadwal <97440789+akshit-dhadwal@users.noreply.github.com>
2022-02-07 16:10:02 +0530

    Student_Record

---
+++ Student_Record/Controllers/StudentController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Caching.Memory;
+using Student_Record.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Student_Record.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<Student>
+        {
+            new Student() {
+                Roll = 1,
+                Name = "AKSHIT",
+                Age = 15,
+            },
+            new Student() {
+                Roll = 2,
+                Name = "AJAY",
+                Age = 16,
+            },
+            new Student() {
+                Roll = 3,
+                Name = "ABHISHEK",
+                Age = 17,
+            },
+            new Student() {
+                Roll = 4,
+                Name = "PARAS",
+                Age = 18,
+            },
+        };
+        
+        public IActionResult Insert()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Insert(Student st)
+        {
+            studentList.Add(st);
+            return RedirectToAction("StudentDetail");
+        }
+        public IActionResult StudentDetail()
+        {
+            
+            return View(studentList.OrderBy(s => s.Roll).ToList());
+
+        }
+
+        
+
+        public IActionResult Edit(int id)
+        {
+
+            var s = studentList.Where(s => s.Roll == id).FirstOrDefault();
+            return View(s);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student st)
+        {
+            var student = studentList.Where(student=>student.Roll == st.Roll).FirstOrDefault();
+            studentList.Remove(student);
+            studentList.Add(st);
+            return RedirectToAction("StudentDetail");
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+
+            var s = studentList.Where(s => s.Roll == id).FirstOrDefault();
+            return View(s);
+        }
+        [HttpPost]
+        public IActionResult Delete(Student st)
+        {
+            var student = studentList.Where(student => student.Roll == st.Roll).FirstOrDefault();
+            studentList.Remove(student);
+            return RedirectToAction("StudentDetail");
+        }
+    }
+}
---
+++ Student_Record/Models/StudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Student_Record.Models
+{
+    public class StudentViewModel
+    {
+       
+    }
+    public class Student
+    {
+        [Display(Name = "RollNumber")]
+        public int Roll { get; set; }
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+      
+    }
+}
